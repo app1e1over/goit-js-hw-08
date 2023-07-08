7601bd801937c04cf6266f82e594ca21adf29582
Orokana seikō <st07.kish@gmail.com>
2023-07-08 11:57:46 +0300

    stuff

---
+++ src/js/01-gallery.js
@@ -0,0 +1,43 @@
+// Описаний в документації
+import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
+// Add imports above this line
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+console.log(galleryItems)
+
+
+const container = document.querySelector(".gallery");
+container.innerHTML = galleryItems
+.map((el) => {
+  const cont = document.createElement("li");
+  cont.classList.add("gallery__item");
+
+  const a = document.createElement("a");
+  a.href = el.original;
+  a.classList.add("gallery__link");
+
+  const img = document.createElement("img");
+  img.classList.add("gallery__image");
+  img.src = el.preview;
+  img.alt = el.description;
+
+  a.appendChild(img);
+  cont.appendChild(a);
+  return cont.outerHTML;
+})
+.join("");
+
+let images = container.getElementsByTagName("li");
+for (let i = 0; i < images.length; i++) {
+  const image = images.item(i);
+  image.addEventListener("click", (e) => {
+    lightbox.open(e.target);
+  });
+}
+
+var lightbox = new SimpleLightbox('.gallery a',{
+  captionsData: "alt",
+  captionDelay:250
+});
